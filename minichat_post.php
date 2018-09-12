c28e8ea8fa86b946829108f8e89f719fb6a6d5db
Jessy Fouace <jessy.fouace62@gmail.com>
2018-09-12 14:03:12 +0200

    Add possibility of add apostrophe

--- minichat_post.php
+++ minichat_post.php
@@ -1,8 +1,8 @@
 <?php require("config.php"); ?>
 <?php
 // Set a variable for the POST
-$pseudo = strip_tags($_POST["pseudo"]);
-$message = strip_tags($_POST["message"]);
+$pseudo = addslashes(strip_tags($_POST["pseudo"]));
+$message = addslashes(strip_tags($_POST["message"]));
 // Protect send on bdd
 if ($pseudo !== "" && $message !== "") {
   $bdd->exec("INSERT INTO chat (pseudo, message) VALUES ('$pseudo', '$message')");
